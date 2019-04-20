f29ee09552636cd90282bec86c29cb1aefdbb6f8
kalle hålldén <kallehalldeen@gmail.com>
2019-04-20 12:37:36 +0200

    Note taking functionality added to api and database

--- src/app.py
+++ src/app.py
@@ -4,14 +4,15 @@ import os
 
 app = Flask(__name__)
 
+count=0
+
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import User
+from models import User, Note, Notepad, Notepad_Notes
 
 @app.route("/")
-
 def hello():
     return "Hello world"
 
@@ -22,13 +23,44 @@ def signup_user():
     try: 
         user=User(
             name=name,
-            password=password
+            password=password,
         )
+        user.note_book = user.id
         db.session.add(user)
         db.session.commit()
         return "Welcome {}".format(user.name)
     except Exception as e:
         return (str(e))
+
+@app.route("/<id_>/add/notepad")
+def add_notepad(id_):
+    try: 
+        user=User.query.filter_by(id=id_).first()
+        notepad=Notepad(
+            user=user.id,
+            person=user
+        )
+        db.session.add(notepad)
+        db.session.commit()
+        return "Added {}".format(notepad.user)
+    except Exception as e:
+        return (str(e))
+
+@app.route("/<id_>/add/note")
+def get_by_noteid(id_):
+        try:
+            notepad= Notepad.query.filter_by(user=id_).first()
+            note = Note(
+                title="title",
+                text="text",
+                notepad = notepad
+            )
+            db.session.add(note)
+            notepad.notes.append(note)
+            db.session.commit()
+            return "Yes"
+        except Exception as e:
+            return (str(e))
 
 @app.route("/showusers")
 def show_users():
@@ -47,15 +79,8 @@ def get_by_id(id_):
         return (str(e))
 
 @app.route("/name/<name>")
-
 def get_book_name(name):
     return "name : {}".format(name)
-
-@app.route("/details")
-def get_bok_details():
-    author = request.args.get('author')
-    published = request.args.get('pubished')
-    return "Author: {}, Published: {}".format(author,published)
 
 
 @app.route("/users")
--- src/models.py
+++ src/models.py
@@ -1,5 +1,9 @@
 
+from flask import Flask, request, jsonify
 from app import db
+from sqlalchemy import Integer, ForeignKey, String, Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 class User(db.Model):
@@ -20,9 +24,87 @@ class User(db.Model):
         return {
             'id' : self.id,
             'name' : self.name,
-            'password' : self.password
+            'password' : self.password,
         }
 
+class Notepad(db.Model):
+    __tablename__ = 'notepad'
+
+    note_pad_id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, db.ForeignKey('users.id'))
+    person = db.relationship(User)
+    notes = []
+
+    def __init__(self, user, person):
+        self.user = user
+        self.person = person
+
+    def __repr__(self):
+        return '<note_pad_id {}>'.format(self.note_pad_id)
+    
+    def serialize(self):
+        return {
+            'user' : self.user,
+            'note_pad_id' : self.note_pad_id,
+            'notes' : self.notes,
+            'person' : self.person
+        }
+    
+    def add_note(title, text):
+        try:
+            note = Note(
+                title=title,
+                text=text
+            )
+            db.session.add(note)
+            db.session.commit()
+            notes.append(note)
+            return jsonify(note.serialize())
+        except Exception as e:
+            return (str(e))
+
+class Note(db.Model):
+    __tablename__ = 'note'
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String())
+    title = db.Column(db.String())
+    notepad_id = db.Column(db.Integer, db.ForeignKey('notepad.note_pad_id'))
+    notepad = db.relationship(Notepad)
+
+    def __init__(self, title, text, notepad):
+        self.text = text
+        self.title = title
+        self.notepad = notepad
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'text' : self.text,
+            'title' : self.title,
+            'notepad' : self.notepad,
+            'notepad_id' : self.notepad_id
+        }
+
+class Notepad_Notes(db.Model):
+    __tablename__ = 'notepad_notes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    note_pad_id = db.Column(db.Integer, db.ForeignKey('notepad.note_pad_id'))
+    note_id = db.Column(db.Integer, db.ForeignKey('note.id'))
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'note_id' : self.note_id,
+            'note_pad_id' : self.note_pad_id,
+        }
  
 
 
